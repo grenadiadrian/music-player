b61f14ad5a116d6977fc406da99569d69a27f80d
Adrian Mason <adrianlmason@gmail.com>
2018-10-28 09:37:02 -0500

    Finished music player app

---
+++ js/data/songs.js
@@ -0,0 +1,50 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/failures-no-option.mp3",
+    "time": "03:17",
+    "title": "Failure's No Option",
+    "artist": "Lloyd Banks"
+    },
+    {
+    "id": "2",
+    "url": "../../music/cant-explain-it.mp3",
+    "time": "04:13",
+    "title": "Can't Explain It",
+    "artist": "LL Cool J"
+    },
+    {
+    "id": "3",
+    "url": "../../music/partners-for-life.mp3",
+    "time": "04:32",
+    "title": "Partners for Life",
+    "artist": "P. Diddy"
+    },
+    {
+    "id": "4",
+    "url": "../../music/tour-stories.mp3",
+    "time": "02:59",
+    "title": "Tour Stories",
+    "artist": "Lloyd Banks"
+    },
+    {
+    "id": "5",
+    "url": "../../music/war-of-the-hearts.mp3",
+    "time": "06:48",
+    "title": "War of the Hearts",
+    "artist": "Sadè"
+    },
+    {
+    "id": "6",
+    "url": "../../music/seven-days.mp3",
+    "time": "03:55",
+    "title": "Seven Days",
+    "artist": "Craig David"
+    },
+    {
+    "id": "7",
+    "url": "../../music/one-call-away.mp3",
+    "time": "04:37",
+    "title": "One Call Away",
+    "artist": "Chingy ft. J-Weav"
+    },
+]
